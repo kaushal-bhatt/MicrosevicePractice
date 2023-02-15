f1be3ed7f54045a739fdaf9ed5004830ca420e50
kaushal <kaushal8650@gmail.com>
2023-02-15 11:31:04 +0530

    initial commit

---
+++ src/main/java/com/microservice/springbootActionBook/SpringbootActionBookApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.springbootActionBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActionBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootActionBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/springbootActionBook/SpringbootActionBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.springbootActionBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActionBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
