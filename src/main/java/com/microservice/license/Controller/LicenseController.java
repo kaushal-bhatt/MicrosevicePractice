39491f6c78508848b91a8390d30237dd86220fc8
sg-kbhatt <kbhatt@sgintl.com>
2023-02-21 12:06:46 +0530

    License controller and services added for crud operation

--- src/main/java/com/microservice/license/Controller/LicenseController.java
+++ src/main/java/com/microservice/license/Controller/LicenseController.java
@@ -1,7 +1,11 @@
 package com.microservice.license.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.microservice.license.Model.License;
+import com.microservice.license.service.Impl.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
@@ -10,5 +14,32 @@ public class LicenseController {
     @RequestMapping(value = "/method")
     String myMethod(){
         return "abba";
+    }
+
+    @Autowired
+    LicenseService licenseService;
+    @GetMapping(value = "/{licenseId}")
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,@PathVariable("licenseId") String licenseId){
+        License license= licenseService.getLicense(licenseId,organizationId);
+        return new ResponseEntity<>(license, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> CreateLicense(@PathVariable("organizationId") String organizationId,@RequestBody License license){
+        String object= licenseService.createLicense(license,organizationId);
+        return new ResponseEntity<>(object, HttpStatus.OK);
+    }
+
+    @PutMapping()
+    public ResponseEntity<String> UpdateLicense(@PathVariable("organizationId") String organizationId,@RequestBody License license){
+        String object= licenseService.updateLicense(license,organizationId);
+        return new ResponseEntity<>(object, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping(value = "/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId,@PathVariable("licenseId") String licenseId){
+        String license= licenseService.deleteLicense(licenseId,organizationId);
+        return new ResponseEntity<>(license, HttpStatus.OK);
     }
 }
